a3e268581b4f183426d8e447f46a635651aeb763
aibrahim <ahmadji.ibr11@gmail.com>
2024-03-03 02:01:35 +0100

    refactor change from ts to js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,23 @@
 import * as THREE from "three";
 
 // scene
-const scene: THREE.Scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 // Mesh1
-const geometry1: THREE.BoxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material1: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: "yellow"});
-const cube1: THREE.Mesh = new THREE.Mesh(geometry1, material1);
+const geometry1 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material1 = new THREE.MeshBasicMaterial({color: "yellow"});
+const cube1 = new THREE.Mesh(geometry1, material1);
 cube1.position.set(-2, 1, 0);
 
 scene.add(cube1);
 
-interface IAspect {
-    width: number;
-    height: number;
-}
-
 // Camera
-const aspect: IAspect = {
+const aspect = {
     width: window.innerWidth,
     height: window.innerHeight
 };
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
 camera.position.z = 3;
 scene.add(camera);
 
@@ -37,7 +32,7 @@ const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(aspect.width, aspect.height);
 
 // Animation
-const animate = (): void => {
+const animate = () => {
 
     renderer.render(scene, camera);
 
